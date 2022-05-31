8283ecd65225937a63b6de0b5214b2e273537cb1
Ben <bentidball97@gmail.com>
2022-05-31 16:57:48 +0100

    can now display api data to frontend

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 import fetchClassInfo from "./api/api.js";
 
+const searchBtn = document.querySelector('#search-btn')
+searchBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    fetchClassInfo();
+})
+
 function resultTemplate(data){
     const result = document.createElement('div');
     result.className = "result";
 
-    const para = document.createElement('p');
-    para.textContent = data.name;
+    const para1 = document.createElement('p');
+    const para2 = document.createElement('p');
 
-    result.appendChild(para);
+    para1.textContent = `Class: ${data.name}, hit die: ${data.hitDie}`;
+    para2.textContent = `Starting gear: ${data.startingEquipment}`;
+
+    result.appendChild(para1);
+    result.appendChild(para2);
+
+    const custom_style={
+        border: "2px solid black",
+        width: "50%",
+        marginTop: "20px",
+    }
+    
+    Object.assign(result.style,custom_style);
     document.body.appendChild(result);
 }
 
-fetchClassInfo();
 
 export default resultTemplate;
